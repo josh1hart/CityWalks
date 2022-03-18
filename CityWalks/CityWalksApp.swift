ce0447adf73897e649adacb77eeb7da5c4afc116
josh1hart <josh1hart@hotmail.co.uk>
2022-03-18 12:12:30 +0000

    Initial Commit

---
+++ CityWalks/CityWalksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityWalksApp.swift
+//  CityWalks
+//
+//  Created by Josh Hart on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CityWalksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityWalks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CityWalks
+//
+//  Created by Josh Hart on 18/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
